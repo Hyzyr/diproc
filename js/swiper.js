ab68e1838e39105c7bdf4358be4ee8eed0e2961f
Ahmet <ahmetcanlyev@gmail.com>
2023-07-24 14:32:26 +0500

    asd

---
+++ js/swiper.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  slidesPerView: "auto",
+  // autoplay: true,
+  speed: 3000,
+  // autoplay: {
+  //   delay: 0,
+  //   disableOnInteraction: false,
+  //   pauseOnMouseEnter: true,
+  //   stopOnLastSlide: false,
+  //   waitForTransition: false,
+  // },
+});
