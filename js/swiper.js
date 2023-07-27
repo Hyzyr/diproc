39d4e89e63f98e99b3a44caaabbc2740a426e7d6
Ahmet <ahmetcanlyev@gmail.com>
2023-07-27 19:45:35 +0500

    diproc

--- js/swiper.js
+++ js/swiper.js
@@ -1,12 +1,12 @@
 const swiper = new Swiper(".swiper", {
   loop: true,
-  slidesPerView: 1,
+  slidesPerView: "auto",
   speed: 300,
-  effect: 'fade',
-  fadeEffect: { crossFade: true },
-  virtualTranslate: true,
+  // effect: "fade",
+  // fadeEffect: { crossFade: true },
+  // virtualTranslate: true,
   navigation: {
-    nextEl: '.slider-next',
-    prevEl: '.slider-prev',
+    nextEl: ".slider-next",
+    prevEl: ".slider-prev",
   },
 });
